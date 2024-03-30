64d0658b417bb7078fd2c29d59016f97960d2749
Ana-Maria Cicoare <anacicoare03@gmail.com>
2024-03-30 21:44:28 +0200

    Implement logging service and update README link

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 ## Documentation:
-- Logger service: https://flask.palletsprojects.com/en/2.3.x/logging/
+- Logger service: https://medium.com/@briankworld/logging-in-flask-introduction-and-practical-example-d2eeac0078b0
--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,25 @@
 from flask import Flask
 from app.data_ingestor import DataIngestor
 from app.task_runner import ThreadPool
+import logging
+from datetime import datetime
 
 webserver = Flask(__name__)
+
+# Initialize the logger & add handler to webserver.log
+webserver.logger.setLevel(logging.INFO)
+handler = logging.FileHandler("webserver.log")
+webserver.logger.addHandler(handler)
+
 webserver.tasks_runner = ThreadPool()
+webserver.logger.info(f"[{datetime.now()}] ThreadPool initialized")
 
 # webserver.task_runner.start()
 
 webserver.data_ingestor = DataIngestor("./nutrition_activity_obesity_usa_subset.csv")
+webserver.logger.info(f"[{datetime.now()}] Loaded data from CSV file")
 
 webserver.job_counter = 1
+
 
 from app import routes
